195c1d4a1155ff1f95445884c48129316cd8da21
Kinga Woch <kinga-niecko@o2.pl>
2023-03-17 18:30:52 +0100

    range

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+const range = document.getElementById("range");
+const numberOfViews = document.getElementById("number-of-views");
+const price = document.querySelector(".price");
 const slider = document.querySelector(".slider");
 const discount = document.querySelector(".discount");
 
 slider.addEventListener("click", () => {
-  if (!discount.classList.contains("active")) {
-    discount.classList.add("active");
+  if (discount.classList.contains("d-none")) {
+    discount.classList.remove("d-none");
   } else {
-    discount.classList.remove("active");
+    discount.classList.add("d-none");
+  }
+});
+
+range.addEventListener("click", () => {
+  console.log(range.value);
+  if (range.value == 1) {
+    numberOfViews.innerHTML = "10K";
+  } else if (range.value == 2) {
+    numberOfViews.innerHTML = "50K";
+  } else if (range.value == 3) {
+    numberOfViews.innerHTML = "100K";
+  } else if (range.value == 4) {
+    numberOfViews.innerHTML = "500K";
+  } else if (range.value == 5) {
+    numberOfViews.innerHTML = "1M";
   }
 });
