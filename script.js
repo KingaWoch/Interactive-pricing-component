7d88552a24e6559aeb0d4dcd7c12fc7eedcb07d7
Kinga Woch <kinga-niecko@o2.pl>
2023-03-17 19:28:08 +0100

    range

--- script.js
+++ script.js
@@ -4,25 +4,55 @@ const price = document.querySelector(".price");
 const slider = document.querySelector(".slider");
 const discount = document.querySelector(".discount");
 
-slider.addEventListener("click", () => {
-  if (discount.classList.contains("d-none")) {
-    discount.classList.remove("d-none");
-  } else {
-    discount.classList.add("d-none");
+range.addEventListener("click", () => {
+  //console.log(range.value);
+  if (range.value == 1) {
+    numberOfViews.innerHTML = "10K";
+    price.innerHTML = "$8.00";
+  } else if (range.value == 2) {
+    numberOfViews.innerHTML = "50K";
+    price.innerHTML = "$12.00";
+  } else if (range.value == 3) {
+    numberOfViews.innerHTML = "100K";
+    price.innerHTML = "$16.00";
+  } else if (range.value == 4) {
+    numberOfViews.innerHTML = "500K";
+    price.innerHTML = "$24.00";
+  } else if (range.value == 5) {
+    numberOfViews.innerHTML = "1M";
+    price.innerHTML = "$36.00";
   }
 });
 
-range.addEventListener("click", () => {
-  console.log(range.value);
-  if (range.value == 1) {
-    numberOfViews.innerHTML = "10K";
-  } else if (range.value == 2) {
-    numberOfViews.innerHTML = "50K";
-  } else if (range.value == 3) {
-    numberOfViews.innerHTML = "100K";
-  } else if (range.value == 4) {
-    numberOfViews.innerHTML = "500K";
-  } else if (range.value == 5) {
-    numberOfViews.innerHTML = "1M";
+slider.addEventListener("click", () => {
+  const val = document.querySelector("input[type=checkbox]").checked;
+  if (val) {
+    discount.classList.add("d-none");
+    if (range.value == 1) {
+      numberOfViews.innerHTML = "10K";
+      price.innerHTML = "$8.00";
+    } else if (range.value == 2) {
+      numberOfViews.innerHTML = "50K";
+      price.innerHTML = "$12.00";
+    } else if (range.value == 3) {
+      numberOfViews.innerHTML = "100K";
+      price.innerHTML = "$16.00";
+    } else if (range.value == 4) {
+      numberOfViews.innerHTML = "500K";
+      price.innerHTML = "$24.00";
+    } else if (range.value == 5) {
+      numberOfViews.innerHTML = "1M";
+      price.innerHTML = "$36.00";
+    }
+  } else {
+    discount.classList.remove("d-none");
+    addDiscount();
   }
 });
+
+function addDiscount() {
+  price.innerHTML =
+    "$" +
+    (price.innerHTML.substring(1) - price.innerHTML.substring(1) * 0.25) +
+    ".00";
+}
